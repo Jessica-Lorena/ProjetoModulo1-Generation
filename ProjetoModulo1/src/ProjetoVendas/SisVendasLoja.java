422a38e542cd188dc8ef730b8da64b908c798f63
Jessica Cristina dos Santos Lorena <jessicacslorena@gmail.com>
2020-08-19 15:31:48 -0300

    Versão 1

---
+++ ProjetoModulo1/src/ProjetoVendas/SisVendasLoja.java
@@ -0,0 +1,14 @@
+package ProjetoVendas;
+
+public class SisVendasLoja {
+
+	protected String nomeLoja;
+	protected long cnpjLoja;
+	
+	public SisVendasLoja(String nomeLoja,long cnpjLoja)
+	{
+		this.nomeLoja = nomeLoja;
+		this.cnpjLoja = cnpjLoja;
+	}
+
+}
